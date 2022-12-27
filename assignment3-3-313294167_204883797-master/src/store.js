0a47ea19b5c8787e56d4633a879f4e4ee67bcbc9
hallelhel <63493176+leahmashi@users.noreply.github.com>
2022-12-27 18:06:05 +0200

    trying to change server address

--- assignment3-3-313294167_204883797-master/src/store.js
+++ assignment3-3-313294167_204883797-master/src/store.js
@@ -5,7 +5,7 @@ const state = {
 
     // server_domain: "http://132.72.65.211/",
     
-    server_domain: "https://recipewizards.up.railway.app:443"
+    server_domain: "https://recipewizards.up.railway.app"
     // server_domain: "https://recipeWizard.cs.bgu.ac.il"
 };
 
